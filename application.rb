d1674bc2dd50bfd5232c18a5150cd29491864a84
Collin Reisdorf <creisdorf@toth.com>
2012-05-01 16:21:46 -0400

    doh fixed the callback ref

--- application.rb
+++ application.rb
@@ -34,7 +34,7 @@ get '/oauth/connect' do
 end
 
 get '/oauth/callback' do
-  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  response = Instagram.get_access_token(params[:code], :redirect_uri => $CALLBACK_URL)
   session[:access_token] = response.access_token
   redirect "/feed"
 end
